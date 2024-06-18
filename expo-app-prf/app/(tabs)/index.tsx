067b0fc2536ed46165044904672e6f3d9a8afc74
gganster <guilian.ganster@gmail.com>
2024-06-18 13:50:59 +0200

    luxon chrono

--- expo-app-prf/app/(tabs)/index.tsx
+++ expo-app-prf/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {StyleSheet, View, ScrollView, Text, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import MyFirstComponent from '@/components/MyFirstComponent';
